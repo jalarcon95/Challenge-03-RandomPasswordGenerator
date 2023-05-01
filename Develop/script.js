f5faa0cab9f41894350c496091a3be022228773e
jalarcon95 <130102758+jalarcon95@users.noreply.github.com>
2023-04-30 21:40:01 -0400

    Added pseudocode to help set up steps toward solution

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log("Hey! You clicked the button!")
+ 
+// 1. Prompt the user for the password criteria
+//    a. password length 8-128
+//    b. lowercase, uppercase, special characters
+// 2. Validate the input
+// 3. Generate password based on criteria
+
+
+
+// 4. Display passwrod on the page.
+  return "Generated password will go here!";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
